76b860130ca1a40bc61261da24635d234ede689e
zhujiajia <772978409@qq.com>
2017-09-18 15:07:47 +0800

    新增无线配置基本设置

--- html_mini/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by zjj on 2017/7/3.
- */
-$(document).ready(function () {
-    $("a[name=networkset]").click(function () {
-        window.location.href = "networksetting.html?tab=" + $(this).attr("id");
-    })
-})
